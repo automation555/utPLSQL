7d5c012582fccfefd75e4d7465f2f9644809654f
Jacek Gębal <jgebal@users.noreply.github.com>
2018-10-28 08:25:59 +0000

    Update download.sh

--- .travis/download.sh
+++ .travis/download.sh
@@ -12,8 +12,10 @@ PRODUCT=""
 # Then download the file using curl.
 downloadFile() {
     downloadUrl=$(exec casperjs download.js $ORACLE_OTN_USER $ORACLE_OTN_PASSWORD $1 $2)
+    printf %s "$URL" | xxd
+    downloadUrl=${downloadUrl%$'\r'}
     echo "DownloadURL: $downloadUrl"
-    curl -o $3 \""${downloadUrl}"\"
+    curl -o $3 \""${url}"\"
 }
 
 #############################
